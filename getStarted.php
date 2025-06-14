a5a7beb9fcb0d5255f4c1ace4a94e5d4cc4976ee
Harsha Samarasinghe <harshasamare25@gmail.com>
2025-06-14 22:45:39 +0530

    2025-06-14

---
+++ getStarted.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get Started</title>
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <!-- swiper js cdn link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <!-- custom css link -->
+    <link rel="stylesheet" href="./Styles/style.css">
+
+    
+</head>
+<body>
+   <header class="header">
+    <a href="index.php" class="logo"><i class="fas fa-hotel"></i> Savendra Garden</a>
+    <nav class="navbar">
+        <a href="index.php">Home</a>
+        <a href="rooms.php">Rooms</a>
+        <a href="payments.php">Payments</a>
+        <a href="helpAndSupport.php">Help & Support</a>
+        <a href="FAQ.php">FAQ</a>
+        <a id="btnGetStarted" href="getStarted.php">Get Started</a>
+        
+    </nav>
+    <div id="menu-btn" ><i class="fas fa-bars"></i></div>
+   </header>
+    
+   
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="./Script/script.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,21 +3,74 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Savendra Gardens</title>
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <!-- swiper js cdn link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <!-- custom css link -->
+    <link rel="stylesheet" href="./Styles/style.css">
+
+    
 </head>
 <body>
-    <form action="index.php" method="post">
-        <lable>User Name: </lable><input type="text" name="username"/><br>
-        <lable>Password: </lable><input type="password" name="password"/>
-        <br>
-        <button type="submit">Login</button>
+   <header class="header">
+    <a href="index.php" class="logo"><i class="fas fa-hotel"></i> Savendra Garden</a>
+    <nav class="navbar">
+        <a href="index.php">Home</a>
+        <a href="rooms.php">Rooms</a>
+        <a href="payments.php">Payments</a>
+        <a href="helpAndSupport.php">Help & Support</a>
+        <a href="FAQ.php">FAQ</a>
+        <a id="btnGetStarted" href="getStarted.php">Get Started</a>
+        
+    </nav>
+    <div id="menu-btn" ><i class="fas fa-bars"></i></div>
 
-    </form>
-    
+
+   </header>
+
+   <section class="home" id="home">
+    <div class="swiper home-slider">
+        <div class="swiper-wrapper">
+        <div class="swiper-slide slide" style="background: url(images/homeSlide01.jpg) no-repeat; background-size: cover;">
+            <div class="content">
+                <h3 class="fade-item">Luxury Living</h3>
+                <h2 class="fade-item">Book hotels instantly from Us <br> Find Your Perfect Stay</h2>
+                <div class="button-group">
+                    <a href="bookNow.php" class="btn fade-item">Book Now</a>
+                    <a href="aboutUs.php" class="btn btn-outline fade-item">Learn More</a>
+                </div>
+            </div>
+        </div>
+         <div class="swiper-slide slide" style="background: url(images/homeSlide02.jpg) no-repeat; background-size: cover;">
+            <div class="content">
+                <h3 class="fade-item">Luxury Living</h3>
+                <h2 class="fade-item">where comfort meets convenience <br> start your journey here</h2>
+                <div class="button-group">
+                    <a href="bookNow.php" class="btn fade-item">Book Now</a>
+                    <a href="aboutUs.php" class="btn btn-outline fade-item">Learn More</a>
+                </div>
+            </div>
+        </div>
+         <div class="swiper-slide slide" style="background: url(images/homeSlide03.jpg) no-repeat; background-size: cover;">
+            <div class="content">
+                <h3 class="fade-item">Luxury Living</h3>
+                <h2 class="fade-item">your next adventure starts with <br> the right room</h2>
+                <div class="button-group">
+                    <a href="bookNow.php" class="btn fade-item">Book Now</a>
+                    <a href="aboutUs.php" class="btn btn-outline fade-item">Learn More</a>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+    </div>
+
+   </section>
+ 
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="./Script/script.js"></script>
 </body>
 </html>
-<?php
-    echo"<h1>Below is your login Details.</h1><br>";
-    echo "User Name: {$_POST["username"]}<br>";
-    echo "Password: {$_POST["password"]}<br>";
-?>
